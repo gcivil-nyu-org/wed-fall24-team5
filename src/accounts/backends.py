76c93e36bc63b58a3e2a817226503ff474c10bb4
TekuriSaiAkhil <55195454+TekuriSaiAkhil@users.noreply.github.com>
2024-10-08 18:05:42 -0400

    Feature/issue 21/extends issue 22 user registration login (#56)
    * Login,Register,Google Auth
    
    * Error message handling
    
    * updated requirements
    
    * code cleanup
    
    * code cleanup
    
    * Improve google login, automate goole admin setup
    
    * code clean up
    
    * code clean up
    
    * code clean up
    
    * updated requirements
    
    * fix: automated admin setup
    
    * code cleanup
    
    * code cleanup
    
    * code cleanup
    
    * code cleanup
    
    ---------
    
    Co-authored-by: Tekuri Sai Akhil <st5050@nyu.edu>

---
+++ src/accounts/backends.py
@@ -0,0 +1,24 @@
+# backends.py
+
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class EmailBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            # Authenticate using email instead of username
+            user = User.objects.get(email=username)
+            if user.check_password(password): # pylint: disable=no-else-return
+                return user
+            else:
+                return None
+        except User.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
