c74a51bd7103345c63b7da14519b5ca335174b91
maxgrove42 <131721751+maxgrove42@users.noreply.github.com>
2024-10-07 21:22:18 -0400

    lint fix

--- src/django_management/settings.py
+++ src/django_management/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-bydqct==mg#ii(79dn!=t77we%@1lw+m!fmdxueqo&*nqc_#53
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['food-donation-swe-dev.us-east-1.elasticbeanstalk.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['food-donation-swe-dev.us-east-1.elasticbeanstalk.com',
+                 '127.0.0.1',
+                 'localhost']
 
 # Application definition
 
