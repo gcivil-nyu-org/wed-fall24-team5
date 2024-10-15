56674c2585352d87593700d44177db7249e7aef1
Arsalan Anwar <arsalan.anwar.7777@gmail.com>
2024-10-15 13:00:10 -0400

    Updated URL Path to keep the naming consistent

--- src/recipient_dashboard/urls.py
+++ src/recipient_dashboard/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import recipient_dashboard
 
 urlpatterns = [
-    path("", recipient_dashboard, name="recipient-dashboard"),
+    path("", recipient_dashboard, name="recipient_dashboard"),
 ]
