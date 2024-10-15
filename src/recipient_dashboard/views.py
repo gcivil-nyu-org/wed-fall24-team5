5859a39ff9187e5f5580029148f424cc4812eb7e
Arsalan Anwar <arsalan.anwar.7777@gmail.com>
2024-10-15 00:18:09 -0400

    Added the view function to fetch the list of all donations

--- src/recipient_dashboard/views.py
+++ src/recipient_dashboard/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render  # noqa
+from database.models import Donation
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def recipient_dashboard(request):
+    donations = Donation.objects.filter(active=True).order_by("created_at")
+    return render(
+        request, "recipient_dashboard/dashboard.html", {"donations": donations}
+    )
