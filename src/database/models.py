f59ec4de7ead6b93961dd646026966a5a1f35374
QahtanAlJammali <qahtan@nyu.edu>
2024-10-17 17:16:07 -0400

    hopefully final change to database until we figure out messages

--- src/database/models.py
+++ src/database/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 import uuid
 
 
@@ -39,6 +41,13 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    else:
+        instance.userprofile.save()
 
 
 class OrganizationAdmin(models.Model):
