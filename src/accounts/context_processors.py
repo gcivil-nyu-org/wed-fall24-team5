4c39ff55cccf38fbd1ef1b9753c9f9f79bf346e2
Tekuri Sai Akhil <saiakhilt50@gmail.com>
2024-10-17 19:30:36 -0400

    view organization page

--- src/accounts/context_processors.py
+++ src/accounts/context_processors.py
@@ -1,10 +1,11 @@
-from database.models import OrganizationAdmin
+from database.models import OrganizationAdmin, User
 
 
 def user_organizations(request):
     if request.user.is_authenticated:
         # Get all organizations the logged-in user belongs to
-        org_admins = OrganizationAdmin.objects.filter(user_email=request.user.email)
+        org_user = User.objects.get(email=request.user.email)
+        org_admins = OrganizationAdmin.objects.filter(user=org_user)
         organizations = [org_admin.organization_id for org_admin in org_admins]
     else:
         organizations = []
