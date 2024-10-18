084ed07a0cb8ca625aa77f320b4f094eac44fa07
maxgrove42 <131721751+maxgrove42@users.noreply.github.com>
2024-10-18 15:53:13 -0400

    lost the DEBUG requirement somewhere along the way

--- src/django_management/settings.py
+++ src/django_management/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # Load DEBUG from environment variables, default to False if not set
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
     "food-donation-swe-dev.us-east-1.elasticbeanstalk.com",
