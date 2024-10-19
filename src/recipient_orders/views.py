7b85ec3055fc812a919b69f996fcf232d7f5f219
maxgrove42 <131721751+maxgrove42@users.noreply.github.com>
2024-10-19 17:32:08 -0400

    modify to include pickup, canceled, pending orders and add tests

--- src/recipient_orders/views.py
+++ src/recipient_orders/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
 from database.models import Order
 
-# Create your views here.
 @login_required
 def recipient_orders(request):
-    # Select all donations that the logged-in user has reserved
-    orders = Order.objects\
-        .filter(user=request.user, active=True)\
+    # Get orders grouped by their status
+    pending_orders = Order.objects.filter(user=request.user,
+                                          active=True,
+                                          order_status="pending")\
         .select_related('donation')\
         .order_by('order_created_at')
-    
-    return render(
-        request, "recipient_orders/orders.html", {'orders': orders}
-    )
+    picked_up_orders = Order.objects.filter(user=request.user,
+                                            active=True,
+                                            order_status="picked_up")\
+        .select_related('donation')\
+        .order_by('order_created_at')
+    canceled_orders = Order.objects.filter(user=request.user,
+                                           active=True,
+                                           order_status="canceled")\
+        .select_related('donation')\
+        .order_by('order_created_at')
+
+    # Pass the grouped orders to the template
+    context = {
+        'pending_orders': pending_orders,
+        'picked_up_orders': picked_up_orders,
+        'canceled_orders': canceled_orders,
+    }
+
+    return render(request, "recipient_orders/orders.html", context)
