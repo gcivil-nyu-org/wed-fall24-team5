9a2456e2bd763928334502c7bf9e84d2a67b7a58
Arsalan Anwar <arsalan.anwar.7777@gmail.com>
2024-10-20 02:41:25 -0400

    Updated tests for recipient dashboard

--- src/recipient_dashboard/tests.py
+++ src/recipient_dashboard/tests.py
@@ -27,14 +27,14 @@ class RecipientDashboardViewTests(TestCase):
 
         # Create active donations for testing
         self.donation1 = Donation.objects.create(
-            organization_id=self.organization,
+            organization_id=self.organization.organization_id,
             food_item="Biryani",
             quantity=10,
             pickup_by="2024-10-15",
             active=True,
         )
         self.donation2 = Donation.objects.create(
-            organization_id=self.organization,
+            organization_id=self.organization.organization_id,
             food_item="Shawarma",
             quantity=5,
             pickup_by="2024-10-15",
