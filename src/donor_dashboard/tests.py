599e964137e893477f80c7ac2cf807a3c72667f6
Arsalan Anwar <49951701+arsalananwar11@users.noreply.github.com>
2024-10-22 10:37:01 -0400

    Implemented test cases for Add Donation functionality (#130)

--- src/donor_dashboard/tests.py
+++ src/donor_dashboard/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
-from database.models import Organization, OrganizationAdmin
+from django.utils import timezone
+from database.models import Organization, OrganizationAdmin, Donation
 from donor_dashboard.forms import AddOrganizationForm
 
 
@@ -113,3 +114,117 @@ class DonorDashboardViewsTests(TestCase):
         self.assertEqual(response.status_code, 302)
         self.organization.refresh_from_db()
         self.assertFalse(self.organization.active)
+
+
+class DonationTests(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        # Create test user and log them in
+        self.user = User.objects.create_user(
+            username="testuser", password="testpassword"
+        )
+        self.client.login(username="testuser", password="testpassword")
+
+        # Create test organization
+        self.organization = Organization.objects.create(
+            organization_name="Test Organization",
+            type="restaurant",
+            address="123 Test St",
+            zipcode=12345,
+        )
+
+        # Create a donation
+        self.donation = Donation.objects.create(
+            food_item="Test Food",
+            quantity=10,
+            pickup_by=timezone.now().date(),
+            organization=self.organization,
+        )
+
+    def test_add_donation_success(self):
+        response = self.client.post(
+            reverse("donor_dashboard:add_donation"),
+            {
+                "food_item": "Hyderabadi Biryani",
+                "quantity": 5,
+                "pickup_by": timezone.now().date(),
+                "organization": str(self.organization.organization_id),
+            },
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(
+            Donation.objects.filter(food_item="Hyderabadi Biryani").exists()
+        )
+        self.assertRedirects(
+            response,
+            reverse(
+                "donor_dashboard:manage_organization",
+                args=[self.organization.organization_id],
+            ),
+        )
+
+    def test_add_donation_missing_fields(self):
+        response = self.client.post(
+            reverse("donor_dashboard:add_donation"),
+            {
+                "food_item": "",  # empty value
+                "quantity": 5,
+                "pickup_by": timezone.now().date(),
+                "organization": str(self.organization.organization_id),
+            },
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Donation.objects.filter(food_item="").exists())
+        self.assertRedirects(
+            response,
+            reverse(
+                "donor_dashboard:manage_organization",
+                args=[self.organization.organization_id],
+            ),
+        )
+
+    def test_add_donation_invalid_quantity(self):
+        response = self.client.post(
+            reverse("donor_dashboard:add_donation"),
+            {
+                "food_item": "Italian Pasta",
+                "quantity": -5,  # negative value
+                "pickup_by": timezone.now().date(),
+                "organization": str(self.organization.organization_id),
+            },
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Donation.objects.filter(food_item="").exists())
+        self.assertRedirects(
+            response,
+            reverse(
+                "donor_dashboard:manage_organization",
+                args=[self.organization.organization_id],
+            ),
+        )
+
+    def test_add_donation_invalid_date(self):
+        response = self.client.post(
+            reverse("donor_dashboard:add_donation"),
+            {
+                "food_item": "Italian Pasta",
+                "quantity": 5,
+                "pickup_by": (timezone.now() + timezone.timedelta(days=8)).strftime(
+                    "%Y-%m-%d"
+                ),  # date after 7 day period
+                "organization": str(self.organization.organization_id),
+            },
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Donation.objects.filter(food_item="").exists())
+        self.assertRedirects(
+            response,
+            reverse(
+                "donor_dashboard:manage_organization",
+                args=[self.organization.organization_id],
+            ),
+        )
