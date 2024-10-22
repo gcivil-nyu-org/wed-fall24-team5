ebdb2e93b3bcbba2bc3cf0cca0895125d88c69cf
Max Grove <131721751+maxgrove42@users.noreply.github.com>
2024-10-21 22:32:02 -0400

    updates to tests to support new DB (#123)

--- src/recipient_dashboard/tests.py
+++ src/recipient_dashboard/tests.py
@@ -33,17 +33,17 @@ class RecipientDashboardViewTests(TestCase):
 
         # Create active donations for testing
         self.donation1 = Donation.objects.create(
-            organization_id=self.organization.organization_id,
+            organization=self.organization,
             food_item="Biryani",
             quantity=10,
-            pickup_by="2024-10-15",
+            pickup_by=timezone.now() + timedelta(days=1),
             active=True,
         )
         self.donation2 = Donation.objects.create(
-            organization_id=self.organization.organization_id,
+            organization=self.organization,
             food_item="Shawarma",
             quantity=5,
-            pickup_by="2024-10-15",
+            pickup_by=timezone.now() + timedelta(days=1),
             active=True,
         )
 
