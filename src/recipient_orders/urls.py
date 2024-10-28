4a6300f33ec64a0aaf7cbd352db297550e43833e
QahtanAlJammali <121693178+QahtanAlJammali@users.noreply.github.com>
2024-10-28 00:03:22 -0400

    cancel orders implemented with the right styling and black and flake8… (#151)
    * cancel orders implemented with the right styling and black and flake8 passing
    
    * added styling and fixed black issues
    
    * fixed linting issue
    
    * added favicon to address mini-issue 152
    
    * styling
    
    * styling to use bulma instead of CSS
    
    * djlint bypass for one item
    
    * added modification of order
    
    * black formatting
    
    * flake8 fixes
    
    * travis build issues fix
    
    * type=cron fix to be between quotations
    
    * lint H021 bypass

--- src/recipient_orders/urls.py
+++ src/recipient_orders/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.recipient_orders, name="recipient_orders"),
+    path("cancel/<uuid:order_id>/", views.cancel_order, name="cancel_order"),
+    path("modify/", views.modify_order, name="modify_order"),
 ]
