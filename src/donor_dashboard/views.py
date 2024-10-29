64ffcdd59c04eb845d7a02a4bcb60623809ed1fd
estcha <chang.estella@gmail.com>
2024-10-29 12:10:17 -0400

    linting

--- src/donor_dashboard/views.py
+++ src/donor_dashboard/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import (
     render,
     redirect,
     get_object_or_404,
-    get_list_or_404,
 )  # noqa
 from database.models import Organization, OrganizationAdmin, User, Donation, Order
 from django.contrib import messages
