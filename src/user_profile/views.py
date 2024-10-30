13e8f6d2766538d05b35878df4dea131aaf3d23a
Arsalan Anwar <arsalan.anwar.7777@gmail.com>
2024-10-30 01:16:16 -0400

    Added test cases for user profile management

--- src/user_profile/views.py
+++ src/user_profile/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from database.models import UserProfile
 from django.contrib import messages
+from django.db import transaction
 
 
 @login_required
@@ -13,15 +14,18 @@ def profile_view(request):
             first_name = request.POST.get("first_name")
             last_name = request.POST.get("last_name")
             phone_number = request.POST.get("phone_number")
-            user_profile.user.first_name = first_name
-            user_profile.user.last_name = last_name
-            user_profile.user.save()
-            user_profile.phone_number = phone_number
-            user_profile.save()
+
+            with transaction.atomic():  # Begin a new atomic transaction block
+                user_profile.user.first_name = first_name
+                user_profile.user.last_name = last_name
+                user_profile.user.save()
+                user_profile.phone_number = phone_number
+                user_profile.save()
+                
             messages.success(request, "Profile updated successfully!")
         except Exception as ex:
             messages.warning(
-                request, f"Some error occured while updating your profile! {str(ex)}"
+                request, "Some error occured while updating your profile!"
             )
         return redirect("user_profile:profile")
 
