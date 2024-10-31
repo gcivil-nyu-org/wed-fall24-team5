db23d72bfd8968adfb49cfa2a56ec102c1d914be
maxgrove42 <131721751+maxgrove42@users.noreply.github.com>
2024-10-31 13:51:25 -0400

    update to models

--- src/database/models.py
+++ src/database/models.py
@@ -177,7 +177,6 @@ class DietaryRestriction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     restriction = models.CharField(max_length=40)
     created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.user.username}: {self.restriction}"
