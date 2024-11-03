dc2cbc45062f4191761366b58cbf5ac33234b418
Arsalan Anwar <49951701+arsalananwar11@users.noreply.github.com>
2024-11-03 13:44:26 -0500

    Feature/issue 193/create new chat rooms and list only those chats that the user is a part of (#195)
    * Messaging custom app added
    
    * Adding the Room table and updating the Message table to reference Room as a foreign key
    
    * Create new chat rooms and list only those chats that the user is a part of
    
    * Lint checker updates
    
    * Fixed black issues
    
    * Fixed linter issues

--- src/messaging/urls.py
+++ src/messaging/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "messaging"
 
 urlpatterns = [
-    path("", views.messaging, name="messaging"),
+    path("", views.messaging_view, name="messaging"),
+    path("start-conversation/", views.start_conversation, name="start_conversation"),
 ]
--- src/messaging/views.py
+++ src/messaging/views.py
@@ -1,5 +1,67 @@
-from django.http import JsonResponse
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from database.models import Room, Organization
+from django.contrib import messages
+from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 
 
-def messaging(request):
-    return JsonResponse({"message": "Messaging functionality coming up soon"})
+@login_required
+def messaging_view(request):
+    user_id = request.user.id
+    organizations = Organization.objects.all()
+    rooms = Room.objects.filter(Q(conversor_1_id=user_id) | Q(conversor_2_id=user_id))
+    return render(
+        request,
+        "messaging_dashboard.html",
+        {"organizations": organizations, "rooms": rooms},
+    )
+
+
+@login_required
+def start_conversation(request):
+    if request.method == "POST":
+        sender_id = request.POST["sender_id"]
+        receiver_id = request.POST["receiver_id"]
+        sender_type = request.POST["sender_type"]
+        receiver_type = request.POST["receiver_type"]
+
+        if sender_type == "user" and receiver_type == "organization":
+            sender = User.objects.get(id=sender_id)
+            receiver = Organization.objects.get(organization_id=receiver_id)
+            room_id = f"{sender_id}_{receiver_id}"
+            room_name = f"Chat with {sender.first_name} and {receiver}"
+            conversor_1_type = ContentType.objects.get_for_model(sender)
+            conversor_1_id = sender.id
+            conversor_2_type = ContentType.objects.get_for_model(receiver)
+            conversor_2_id = (receiver.organization_id,)
+
+        elif sender_type == "organization" and receiver_type == "user":
+            sender = User.objects.get(id=sender_id)
+            receiver = Organization.objects.get(organization_id=receiver_id)
+            room_id = f"{receiver_id}_{sender_id}"
+            room_name = f"Chat with {receiver.first_name} and {sender}"
+            conversor_1_type = ContentType.objects.get_for_model(sender)
+            conversor_1_id = sender.organization_id
+            conversor_2_type = ContentType.objects.get_for_model(receiver)
+            conversor_2_id = receiver.id
+
+        try:
+            room = Room.objects.get(room_id=room_id)
+            messages.warning(request, f"Chat already exists with {receiver}")
+        except ObjectDoesNotExist:
+            room = Room(
+                room_id=room_id,
+                room_name=room_name,
+                conversor_1_type=conversor_1_type,
+                conversor_1_id=conversor_1_id,
+                conversor_2_type=conversor_2_type,
+                conversor_2_id=conversor_2_id,
+            )
+            room.save()
+            messages.success(request, f"Created a new chat with {receiver}")
+
+        return redirect("messaging:messaging")
+    return redirect("messaging:messaging")
