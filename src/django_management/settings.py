983f5064c69082d8d176596a563dc6644a658ab7
Tekuri Sai Akhil <saiakhilt50@gmail.com>
2024-11-07 18:49:47 -0500

    roll back

--- src/django_management/settings.py
+++ src/django_management/settings.py
@@ -106,10 +106,7 @@ ASGI_APPLICATION = "django_management.asgi.application"
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],  # replace with your Redis endpoint
-        },
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
     },
 }
 
