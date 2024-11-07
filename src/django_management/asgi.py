fdc19bb73cec316be75bd17a98f75735d49143fc
Tekuri Sai Akhil <saiakhilt50@gmail.com>
2024-11-07 18:59:12 -0500

    remove procfile

--- src/django_management/asgi.py
+++ src/django_management/asgi.py
@@ -8,13 +8,13 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-import django
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from messaging import routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_management.settings")
-django.setup()
+
 django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {"http": django_asgi_app, "websocket": URLRouter(routing.websocket_urlpatterns)}
