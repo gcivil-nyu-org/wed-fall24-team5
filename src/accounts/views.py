5e3bf81688e55d77e5879289a73d440bfafdad6c
Max Grove <131721751+maxgrove42@users.noreply.github.com>
2024-11-15 13:59:18 -0500

    bug fix for better register warnings (#322)
    * bug fix for better register warnings
    
    * linter

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -63,9 +63,18 @@ def register_view(request):
             login(request, user, backend="django.contrib.auth.backends.ModelBackend")
             return redirect("accounts:profile")
         else:
-            messages.warning(
-                request, "Registration Failed!! Password mismatch or too generic"
-            )  # noqa
+            if form.errors.get("password2"):
+                messages.warning(request, "Password mismatch.")
+            elif form.errors.get("password1"):
+                messages.warning(request, "Password is too generic.")
+            elif form.errors.get("email"):
+                messages.warning(
+                    request, "Email is not valid. Please try again with a valid email."
+                )
+            else:
+                messages.warning(
+                    request, "Registration failed. Please check the form for errors."
+                )
             form = MyUserCreationForm()
             return render(request, "accounts/register.html", {"form": form})
     else:
